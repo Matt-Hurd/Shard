e35feac1c5b37e0571f99f90f4d8035f7ad0841c
EpsilonF <greg.ry.mcd@gmail.com>
2014-02-04 10:10:32 -0600

    Finished GameObject

--- Shard/Shard/GameObject.cs
+++ Shard/Shard/GameObject.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Shard
 {
@@ -14,18 +21,20 @@ namespace Shard
         private double rotationVelocity;
         private double health;
 
-        public double GetX() { return this.x; }
-        public double GetY() { return this.y; }
-        public double GetDirection() { return this.direction; }
-        public double GetVelocity() { return this.velocity; }
-        public double GetRotationalVelocity() { return this.rotationVelocity; }
-        public double GetHealth() { return this.health; }
+        #region Mutating and Returning Fields
 
-        public void setX(double x) { this.x = x; }
-        public void setY(double y) { this.y = y; }
-        public void setDirection(double direction) { this.direction = direction; }
-        public void setVelocity(double velocity) { this.velocity = velocity; }
-        public void setRotationalVel(double rotationVelocity) { this.rotationVelocity = rotationVelocity; }
+        //public double GetX() { return this.x; }
+        //public double GetY() { return this.y; }
+        //public double GetDirection() { return this.direction; }
+        //public double GetVelocity() { return this.velocity; }
+        //public double GetRotationalVelocity() { return this.rotationVelocity; }
+        //public double GetHealth() { return this.health; }
+
+        //public void setX(double x) { this.x = x; }
+        //public void setY(double y) { this.y = y; }
+        //public void setDirection(double direction) { this.direction = direction; }
+        //public void setVelocity(double velocity) { this.velocity = velocity; }
+        //public void setRotationalVel(double rotationVelocity) { this.rotationVelocity = rotationVelocity; }
 
         public virtual double X
         {
@@ -98,6 +107,27 @@ namespace Shard
                 health = value;
             }
         }
+
+        #endregion
+
+        //Should be Ovverriden
+        public void move()
+        {
+            this.X += Math.Cos(direction) * velocity;
+            this.Y += Math.Sin(direction) * velocity;
+        }
+        
+        //Should be Overriden 
+        public void update()
+        {
+            move();
+        }
+
+        public abstract Rectangle getBounds();
+
+        public abstract bool intersects(GameObject gameObject);
+
+        public abstract void draw(SpriteBatch spriteBatch, Texture2D spritesheet);
 
     }
 }
