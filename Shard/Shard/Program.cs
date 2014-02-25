d9851b048884e3ac5bf6505a89008c2bfe378678
Abahbob <sabahbob@gmail.com>
2014-02-25 10:00:50 -0600

    and finish fixing it

--- Shard/Shard/Program.cs
+++ Shard/Shard/Program.cs
@@ -14,11 +14,6 @@ namespace Shard
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
-
-            using (ShardGame game = new ShardGame())
-            {
-                game.Run();
-            }
         }
     }
 #endif
